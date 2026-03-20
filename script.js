d4c21ecfc4635b163a3e2cbc54112d697ca2ac98
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-20 23:10:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+const btn = document.getElementById("submit");
+btn.addEventListener('click',(e)=>{
+	// e.preventDefault();
+const firstName = document.getElementById("firstName").value;
+const lastName = document.getElementById("lastName").value;
+const PhoneNumber = document.getElementById("PhoneNumber").value;
+const EmailId = document.getElementById("EmailId").value;
+if (!firstName || !lastName || !PhoneNumber || !EmailId) {
+  alert("Please fill all fields");
+  return; 
+}
+alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${PhoneNumber} Email Id: ${EmailId}`);
+});
